bcb504a64d4d5052d90632cbd6303ff19e0c03c0
yewo <yewomkandawire1@gmail.com>
2025-05-22 13:50:11 +0200

    removed temp.py

--- qt_web_browser/browser.py
+++ qt_web_browser/browser.py
@@ -1,51 +1,67 @@
 import sys
-from PyQt5.QtWidgets import QApplication, QMainWindow, QToolBar, QAction, QLineEdit
+from PyQt5.QtWidgets import QApplication, QWidget, QVBoxLayout, QLineEdit, QPushButton, QLabel, QHBoxLayout
+
 from PyQt5.QtWebEngineWidgets import QWebEngineView
 from PyQt5.QtCore import QUrl
 
-class Browser(QMainWindow):
+class Browser(QWidget):
     def __init__(self):
         super().__init__()
-        self.setWindowTitle("My Python Browser")
-        self.setGeometry(100, 100, 1200, 800)
+        self.setWindowTitle("DevNet Web Browser")
+
+        self.layout = QVBoxLayout()
+        self.setLayout(self.layout)
+
+        # Search/URL Bar
+        self.url_bar = QLineEdit()
+        self.url_bar.setPlaceholderText("Enter page name (e.g., register)")
+        self.url_bar.returnPressed.connect(self.load_page)
 
         self.browser = QWebEngineView()
-        self.browser.setUrl(QUrl("https://www.google.com"))
-        self.setCentralWidget(self.browser)
+        self.error_label = QLabel()
+        self.google_button = QPushButton("Search on Google")
+        self.google_button.clicked.connect(self.search_google)
+        self.google_button.hide()
 
-        # Toolbar
-        nav_bar = QToolBar()
-        self.addToolBar(nav_bar)
+        top_bar = QHBoxLayout()
+        top_bar.addWidget(QLabel("Search:"))
+        top_bar.addWidget(self.url_bar)
 
-        back_btn = QAction("Back", self)
-        back_btn.triggered.connect(self.browser.back)
-        nav_bar.addAction(back_btn)
+        self.layout.addLayout(top_bar)
+        self.layout.addWidget(self.browser)
+        self.layout.addWidget(self.error_label)
+        self.layout.addWidget(self.google_button)
 
-        forward_btn = QAction("Forward", self)
-        forward_btn.triggered.connect(self.browser.forward)
-        nav_bar.addAction(forward_btn)
+        self.load_homepage()
 
-        reload_btn = QAction("Reload", self)
-        reload_btn.triggered.connect(self.browser.reload)
-        nav_bar.addAction(reload_btn)
+    def load_homepage(self):
+        self.browser.setUrl(QUrl("http://localhost:8085/"))
 
-        self.url_bar = QLineEdit()
-        self.url_bar.returnPressed.connect(self.navigate_to_url)
-        nav_bar.addWidget(self.url_bar)
+    def load_page(self):
+        page = self.url_bar.text().strip()
+        if not page:
+            self.load_homepage()
+        else:
+            url = f"http://localhost:8085/{page}"
+            self.browser.setUrl(QUrl(url))
+            self.browser.loadFinished.connect(self.check_page_loaded)
 
-        self.browser.urlChanged.connect(self.update_url)
+    def check_page_loaded(self, ok):
+        if not ok:
+            self.error_label.setText("Page not found. Try searching it online.")
+            self.google_button.show()
+        else:
+            self.error_label.setText("")
+            self.google_button.hide()
 
-    def navigate_to_url(self):
-        url = self.url_bar.text()
-        if not url.startswith("http"):
-            url = "http://" + url
-        self.browser.setUrl(QUrl(url))
+    def search_google(self):
+        query = self.url_bar.text()
+        google_url = f"https://www.google.com/search?q={query}"
+        self.browser.setUrl(QUrl(google_url))
 
-    def update_url(self, q):
-        self.url_bar.setText(q.toString())
-
-app = QApplication(sys.argv)
-QApplication.setApplicationName("Python Browser")
-window = Browser()
-window.show()
-sys.exit(app.exec_())
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    browser = Browser()
+    browser.resize(900, 600)
+    browser.show()
+    sys.exit(app.exec_())
